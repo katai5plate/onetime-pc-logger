ca2e1aec36b08ab2133a02a520f57e81774672b4
katai5plate <katai5plate.sub@gmail.com>
2020-12-13 04:29:50 +0900

    init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # onetime-pc-logger
 
-数秒ごとにPCの状況ログを上書きするPCロガー
+数秒ごとに PC の状況ログを上書きする PC ロガー
 
 ## why?
 
@@ -8,14 +8,23 @@
 
 熱暴走を疑ってる。
 
+## usage
+
+`node .` or `start.bat`
+
+- `npm i` を忘れずに
+- ログは `onetime.log` に吐き出される
+- 5 秒ごとに更新
+
 ## concept
 
 5 秒ごとに以下の情報を取得し、ログファイルを上書きする。
-- CPU温度
+
+- CPU 温度
   - `wmic /namespace:\\root\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature`
-- CPU使用率
+- CPU 使用率
   - `wmic CPU get loadpercentage`
-- GPU状況
+- GPU 状況
   - `nvidia-smi`
 - タスクリスト
   - `tasklist -v`
---
+++ index.js
@@ -0,0 +1,31 @@
+const { writeFileSync } = require("fs");
+const { execSync } = require("child_process");
+const { convert } = require("encoding-japanese");
+
+const exec = (code) =>
+  convert(execSync(code), {
+    from: "SJIS",
+    to: "UNICODE",
+    type: "string",
+  });
+
+const run = () => {
+  const log = [
+    "-- CPU Temperature --",
+    exec(
+      "wmic /namespace:\\\\root\\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature"
+    ),
+    "-- CPU Usage --",
+    exec("wmic CPU get loadpercentage"),
+    "-- GPU --",
+    exec("nvidia-smi"),
+    "-- Tasks --",
+    exec("tasklist -v"),
+  ].join("\n");
+  writeFileSync("./onetime.log", log);
+  console.log(`Saved at ${new Date()}`);
+};
+
+console.log("START!");
+run();
+setInterval(run, 5000);
